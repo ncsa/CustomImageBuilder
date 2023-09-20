5c57ce8265bcd484540af0d09681bc83c7038f8d
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-09-19 20:31:51 -0500

    Prep-for-release
    Add .gitignore
    
    Downgraded globus-compute-endpoint to 2.2.0
    
    Updated example

--- example/example/client.py
+++ example/example/client.py
@@ -1,11 +1,37 @@
 from custom_image_builder import build_and_register_container
+from globus_compute_sdk import Client, Executor
+
+
+def transform():
+    import pandas as pd
+    data = {'Column1': [1, 2, 3],
+            'Column2': [4, 5, 6]}
+
+    df = pd.DataFrame(data)
+
+    return "Successfully created df"
 
 
 def main():
-    container_id = build_and_register_container("01e21ddf-6eb4-41db-8e1d-2bcfe0c8314f",
-                                                "my-test-image", "docker", "python:3.8")
+    image_builder_endpoint = "81b21a94-0e18-457d-98b5-05672a8a3b60"
+    gcc_client = Client()
 
-    print("Container id ", container_id)
+    container_id = build_and_register_container(gcc_client=gcc_client,
+                                                endpoint_id=image_builder_endpoint,
+                                                image_file_name="my-pandas-image",
+                                                base_image_type="docker",
+                                                base_image="python:3.8",
+                                                pip_packages=["pandas"])
+
+    print("The container id is", container_id)
+
+    example_endpoint = "0b4e042b-edd5-4951-9ce5-6608c2ef6cb8"
+
+    with Executor(endpoint_id=example_endpoint,
+                  container_id="791a75b4-c2bd-40f1-85e0-ba17458c233b") as ex:
+        fut = ex.submit(transform)
+
+    print(fut.result())
 
 
 if __name__ == '__main__':
