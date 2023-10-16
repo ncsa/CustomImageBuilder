d60fafa6ecc1b2f748d2a09882bc94e60de7efd0
ritwik-deshpande <ritwikdeshpande01@gmail.com>
2023-10-16 13:47:23 -0500

    Added redef_in_main function to fix dill serialization

--- custom_image_builder/image_builder.py
+++ custom_image_builder/image_builder.py
@@ -1,5 +1,6 @@
-from globus_compute_sdk import Executor, Client
+import inspect
 
+from globus_compute_sdk import Executor, Client
 from custom_image_builder.exception.ImageBuilderException import ImageBuilderException
 from custom_image_builder.exception.RegisterImageException import RegisterImageException
 
@@ -95,6 +96,31 @@ def register_container(image_file_path, gcc):
     except Exception as ex:
         raise RegisterImageException(ex)
 
+def redef_in_main(obj):
+    """Helper: redefine an object in __main__, e.g. garden_ai._Model -> __main__._Model.
+
+    This has the effect of coaxing dill into serializing both the definition and
+    the instance of an object together (in the case of a class), so it can be
+    deserialized without needing the definition to be available for import on
+    the other side. We do this for the "real" function we register, too (see below)
+    in order to guarantee that there are no intra-garden references that dill might
+    try to import on the other end.
+
+    This works because dill is smart enough to know that if you defined a class
+    interactively (like in a repl) then it can't expect that definition to be
+    available after the session exits, so has to include it.
+
+    The upshot is that we can embed stuff like the _Model class as an implicit part of
+    the "function" that we register with globus compute, so there won't be any
+    garden-specific dependencies users need to worry about in the container.
+    """
+
+    # make sure it's not already in main
+    if obj.__module__ != "__main__":
+        import __main__
+
+        s = inspect.getsource(obj)
+        exec(s, __main__.__dict__)
 
 def build_image(gc_executor: Executor,
                 image_file_name: str,
@@ -116,9 +142,12 @@ def build_image(gc_executor: Executor,
     :param base_image_type:
     :return: image_file_path
     """
+    import __main__
+
     try:
         with gc_executor as ex:
-            fut = ex.submit(funcx_build_image,
+            redef_in_main(funcx_build_image)
+            fut = ex.submit(__main__.funcx_build_image,
                                      image_file_name,
                                      base_image_type,
                                      base_image,
